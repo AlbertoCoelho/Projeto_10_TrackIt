6a1be6740d72fbc3441f63f969af58c4e84fa9b8
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-01-14 09:26:24 -0300

    Preparando revisão de código

--- src/components/HabitRegister.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const HabitRegister = () => {
-  return (
-    <h1>Eu sei que deveria ter um registro aqui para fazer o hábito mas não deu tempo, estou mais triste que você pode acreditar :(.</h1>
-  );
-}
-
-export default HabitRegister;
--- src/pages/Historic/index.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Historic = () => {
-  return (
-    <h1>Minha linda página de histórico</h1>
-  );
-}
-
-export default Historic;
